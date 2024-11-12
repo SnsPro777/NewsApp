8c2ccd21565fb88bf7e3e9ee7db88f558508ce0b
scorp <snspro777@gmail.com>
2024-11-12 19:59:33 +0500

    implement OnBoardingScreen

---
+++ app/src/main/java/com/loc/newsapp/domain/usecases/app_entry/SaveAppEntry.kt
@@ -0,0 +1,12 @@
+package com.loc.newsapp.domain.usecases.app_entry
+
+import com.loc.newsapp.domain.manger.LocalUserManger
+
+class SaveAppEntry(
+   private val localUserManger: LocalUserManger
+) {
+
+   suspend operator fun invoke(){
+      localUserManger.saveAppEntry()
+   }
+}
